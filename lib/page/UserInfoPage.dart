afbe04e112fe85d92a832e11e1bec1efb989feb1
devinli <517367150@qq.com>
2019-01-28 18:15:12 +0800

    modify：remove no use code

--- lib/page/UserInfoPage.dart
+++ lib/page/UserInfoPage.dart
@@ -14,37 +14,11 @@ class MyInfoPageState extends State<MyInfoPage> {
   Color themeColor = ThemeUtils.currentColorTheme;
   static const double ARROW_ICON_WIDTH = 30;
   static const double IMAGE_INCON_HEIGHT = 16;
-  var titles = ["我的消息", "阅读记录", "我的博客", "我的问答", "我的活动", "我的团队", "邀请好友"];
-  var imagePaths = [
-    "images/ic_my_message.png",
-    "images/ic_my_blog.png",
-    "images/ic_my_blog.png",
-    "images/ic_my_question.png",
-    "images/ic_discover_pos.png",
-    "images/ic_my_team.png",
-    "images/ic_my_recommend.png"
-  ];
   final double _appBarHeight = 180.0;
-  var icons = [];
-  var userAvatar;
-  var userName;
-  var titleTextStyle = new TextStyle(fontSize: 16.0);
-  var rightArrowIcon = new Image.asset(
-    'images/ic_arrow_right.png',
-    width: ARROW_ICON_WIDTH,
-    height: ARROW_ICON_WIDTH,
-  );
- // final String _userHead =
- //     'http://a3.qpic.cn/psb?/V11GoRzM2Vse5R/*wGBFAcV4X2dSslW6P4t5w2m3Ro0BdkV.vfonfMRyjQ!/b/dCIBAAAAAAAA&ek=1&kp=1&pt=0&bo=QAZSCBAJEAwRED4!&tl=3&vuin=517367150&tm=1548144000&sce=60-2-2&rf=viewer_311';
+
 
   String _userHead =
       "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548653561973&di=e5b294834c9a5891939c8c325b702d1c&imgtype=0&src=http%3A%2F%2Fimg.mp.itc.cn%2Fupload%2F20170720%2F8b000b96022d4ce189717fe93d9e1e0a_th.jpg";
-
-  MyInfoPageState() {
-    for (int i = 0; i < imagePaths.length; i++) {
-      icons.add(getIconImage(imagePaths[i]));
-    }
-  }
 
   Widget getIconImage(path) {
     return new Padding(
@@ -85,77 +59,28 @@ class MyInfoPageState extends State<MyInfoPage> {
                       ),
                     ),
                   ),
-
-                      new Container(
-                        child: new Padding(
-                          padding: const EdgeInsets.only(
-                            top: 40.0,
-                          ),
-                          child: new CircleAvatar(
-                            radius: 35.0,
-                            backgroundImage: new NetworkImage(_userHead),
-                          ),
-                        ),
-                      )
+                  new Container(
+                    child: new Padding(
+                      padding: const EdgeInsets.only(
+                        top: 40.0,
+                      ),
+                      child: new CircleAvatar(
+                        radius: 35.0,
+                        backgroundImage: new NetworkImage(_userHead),
+                      ),
+                    ),
+                  )
                 ],
               ),
             ),
           ),
           new SliverList(
               delegate: new SliverChildListDelegate(<Widget>[
-
-                new Container(
-                  color: Colors.white,
-                  margin: const EdgeInsets.only(top: 10.0),
-                  child: Column(
-                    children: <Widget>[
-                      new MenuItem(
-                        icon: Icons.face,
-                        title: '车辆管理1',
-                      ),
-                      new MenuItem(
-                        icon: Icons.face,
-                        title: '修改历史',
-                      ),
-                      new MenuItem(
-                        icon: Icons.face,
-                        title: '修改PIN码',
-                      ),
-                      new MenuItem(
-                        icon: Icons.face,
-                        title: '修改登陆密码',
-                      ),
-                      new MenuItem(
-                        icon: Icons.face,
-                        title: '修改登陆密码',
-                      ),
-                      new MenuItem(
-                        icon: Icons.face,
-                        title: '修改手机号',
-                      ),
-                      new MenuItem(
-                        icon: Icons.face,
-                        title: '在线客服',
-                      ),
-                      new MenuItem(
-                        icon: Icons.face,
-                        title: '在线客服',
-                      ),
-                      new MenuItem(
-                        icon: Icons.face,
-                        title: '在线客服',
-                      ),
-                      new MenuItem(
-                        icon: Icons.face,
-                        title: '在线客服',
-                      ),
-                      new MenuItem(
-                        icon: Icons.face,
-                        title: '在线客服',
-                      ),
-                    ],
-                  ),
-                )
+            new Container(
+              color: Colors.white,
+              margin: const EdgeInsets.only(top: 10.0),
+              child: Column(children: getMenuListItem()),
+            )
           ]))
         ],
       ),
@@ -171,9 +96,31 @@ class MyInfoPageState extends State<MyInfoPage> {
   //显示用户详细信息
   _showUserInfoDetail() {}
 
-//登陆
+  //登陆
   _login() async {}
 
-  //退出登陆
-  _logout() async {}
+  var _itemdata = [
+    "车辆管理",
+    "修改历史",
+    "修改PIN码",
+    "修改登陆密码",
+    "修改登陆密码",
+    "修改手机号",
+    "在线客服",
+    "在线客服",
+    "在线客服",
+    "在线客服",
+    "在线客服",
+    "在线客服",
+    "在线客服",
+  ];
+
+  List<Widget> getMenuListItem() {
+    List<Widget> widges = new List();
+    for (int i = 0; i < _itemdata.length; i++) {
+      MenuItem item = new MenuItem(icon: Icons.face, title: _itemdata[i]);
+      widges.add(item);
+    }
+    return widges;
+  }
 }
